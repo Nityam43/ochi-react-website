b6dcd7cc68fcc7acb7f161d649eb45f2c126a074
Nityam Savaliya <nityamsavaliya@gmail.com>
2025-06-15 21:50:57 +0530

    commit 003

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,13 @@
+const Marquee = () => {
+  return (
+    
+    <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
+        <div className="text flex gap-10 whitespace-nowrap border-t-2 border-b-2 overflow-hidden border-zinc-300">
+            <h1 className="text-[20vw] leading-none font-[Founders_Grotesk_Condensed] uppercase pt-10 -mb-[6vw] font-semibold">we are ochi</h1>
+            <h1 className="text-[20vw] leading-none font-[Founders_Grotesk_Condensed] uppercase pt-10 -mb-[6vw] font-semibold">we are ochi</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Marquee
